04e34ed0d9b714278a22ef67c6b89bdd3ea11703
maciejwiacek <135031892+maciejwiacek@users.noreply.github.com>
2024-02-26 19:53:27 +0100

    Added libft files

---
+++ ft_isalnum.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mwiacek <mwiacek@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/26 19:23:50 by mwiacek           #+#    #+#             */
+/*   Updated: 2024/02/26 19:30:56 by mwiacek          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+static int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+static int	ft_isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+int	ft_isalnum(int c)
+{
+	if (ft_isdigit(c) || ft_isalpha(c))
+	{
+		return (1);
+	}
+	return (0);
+}
