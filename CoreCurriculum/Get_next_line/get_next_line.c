c32bb64edc95cc001fbf0dab2beff4aa3d8bfe90
maciejwiacek <wiacem811_aehit@students.vizja.pl>
2024-03-18 14:40:20 +0100

    Added files for getnextline

---
+++ CoreCurriculum/Get_next_line/get_next_line.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mwiacek <mwiacek@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/17 23:03:46 by mwiacek           #+#    #+#             */
+/*   Updated: 2024/03/17 23:42:35 by mwiacek          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	char	*buffer;
+	buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	read(fd, buffer, BUFFER_SIZE);
+	return (buffer);
+}
+
+#include <stdio.h>
+#include <fcntl.h>
+
+int main()
+{
+	int fd = open("test.txt", O_RDONLY);
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+}
