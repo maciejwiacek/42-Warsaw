3e2a93a2226cb535aaba3b5a40956618cec4d505
maciejwiacek <maciejwiacek4@gmail.com>
2024-06-13 17:17:32 +0000

    Refactored library

--- library/includes/libft.h
+++ library/includes/libft.h
@@ -16,12 +16,6 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-typedef struct s_list
-{
-	void			*content;
-	struct s_list	*next;
-}	t_list;
-
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
@@ -56,14 +50,5 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 char	*ft_strrchr(const char *s, int c);
 void	ft_putnbr_fd(int n, int fd);
 char	*ft_itoa(int n);
-t_list	*ft_lstnew(void *content);
-void	ft_lstadd_front(t_list **lst, t_list *new);
-int		ft_lstsize(t_list *lst);
-t_list	*ft_lstlast(t_list *lst);
-void	ft_lstadd_back(t_list **lst, t_list *new);
-void	ft_lstdelone(t_list *lst, void (*del)(void*));
-void	ft_lstclear(t_list **lst, void (*del)(void*));
-void	ft_lstiter(t_list *lst, void (*f)(void *));
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
